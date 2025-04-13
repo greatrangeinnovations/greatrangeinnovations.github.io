93e80e8714390bb084d078047e66134ea909d1d4
greatrangeinnovations <166881942+greatrangeinnovations@users.noreply.github.com>
2025-04-13 13:07:08 -0400

    missing packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  resolve: {
+    alias: {
+      "@emotion/react": path.resolve(__dirname, "node_modules/@emotion/react"),
+      "@emotion/styled": path.resolve(
+        __dirname,
+        "node_modules/@emotion/styled"
+      ),
+    },
+  },
 });
